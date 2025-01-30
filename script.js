430ee7d6cf0dbf2cf1bb543b8b6175a087357651
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2025-01-29 20:24:42 -0800

    improved styling when images are uploaded

--- script.js
+++ script.js
@@ -148,3 +148,23 @@ async function queryGPT4o(userMessage, imageBase64) {
         return `Error: ${error.message}`;
     }
 }
+
+function updateIcon() {
+    const fileInput = document.getElementById("imageInput");
+    const fileIcon = document.getElementById("fileIcon");
+
+    if (fileInput.files.length > 0) {
+        fileIcon.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+            </svg>
+        `;
+    } else {
+        fileIcon.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
+            </svg>
+        `;
+    }
+}
